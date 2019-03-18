bd0211c19516629037ea11ea8999767e1e481e71
256hz <a.d@256hz.com>
2019-03-18 15:36:48 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :text, :string
+
+  def initialize(text)
+    @text = text
+  end
+
+  def make_sort(string)
+    text_hash = {}
+    string.split('').map { |letter|
+      text_hash.keys.include?(letter) ? text_hash[letter] += 1 : text_hash[letter] = 1
+    }
+    text_hash.sort
+  end
+
+  def match(string)
+    match_array = []
+    string.each { |word|
+      if make_sort(word) == make_sort(@text)
+        match_array << word
+      end
+    }
+    match_array
+  end
+
+end
+#ana = Anagram.new('allergy')
+
+#pry.Start
